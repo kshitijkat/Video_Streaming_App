d347106c04ff1534acd9326efe685f680d39f352
Kshitij Katiyar <kshitijkatiyar02042001@gmail.com>
2021-01-23 01:49:00 +0530

    First Commit

---
+++ app/src/main/java/com/example/Video_streaming_app/MainActivity.kt
@@ -0,0 +1,13 @@
+package com.example.Video_streaming_app
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Toast
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        Toast.makeText(this, "", Toast.LENGTH_SHORT).show()
+    }
+}
