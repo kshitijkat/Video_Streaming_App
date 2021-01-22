0b2123a041409a2b6f91370202b1d3ce90f1d19c
Kshitij Katiyar <kshitijkatiyar02042001@gmail.com>
2021-01-23 03:04:48 +0530

    Second Commit

--- app/src/main/java/com/example/Video_streaming_app/MainActivity.kt
+++ app/src/main/java/com/example/Video_streaming_app/MainActivity.kt
@@ -1,13 +1,42 @@
 package com.example.Video_streaming_app
 
+import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 import android.widget.Toast
+import kotlinx.android.synthetic.main.activity_main.*
+import android.view.View.VISIBLE as VISIBLE
 
 class MainActivity : AppCompatActivity() {
+    private var playbackPos=0
+    private val url="rtsp://wowzaec2demo.streamlock.net/vod/mp4:BigBuckBunny_115k.mov"
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        Toast.makeText(this, "", Toast.LENGTH_SHORT).show()
+
+        videoView.setOnPreparedListener {
+            videoView.seekTo(playbackPos)
+            videoView.start()
+        }
+
+    }
+
+    override fun onStart() {
+        super.onStart()
+        val uri= Uri.parse(this.url)
+        videoView.setVideoURI(uri)
+        progressBar2.visibility= VISIBLE
+    }
+
+    override fun onPause() {
+        super.onPause()
+        videoView.pause()
+        playbackPos=videoView.currentPosition
+    }
+
+    override fun onStop() {
+        videoView.stopPlayback()
+        super.onStop()
     }
 }
